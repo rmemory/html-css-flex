06fa65a3030864dcb24f0ab94c69d6b681dd6337
Richard Memory <richardfmemory@gmail.com>
2018-03-31 11:08:25 -0600

    Cleanup and additional comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+
+// global selector strings
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
@@ -6,13 +8,27 @@ var HIDDEN_DETAIL_CLASS = 'hidden-detail';
 var TINY_EFFECT_CLASS = 'is-tiny';
 var ESC_KEY = 27;
 
+/*
+ * hideDetails
+ * 
+ * Add the hidden-detail class to the body
+ *
+ */
 function hideDetails() {
   'use strict';
+
   document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
 
+/*
+ * showDetails
+ * 
+ * Show the detail image, with animation
+ *
+ */
 function showDetails() {
   'use strict';
+
   var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
   document.body.classList.remove(HIDDEN_DETAIL_CLASS);
   frame.classList.add(TINY_EFFECT_CLASS);
@@ -21,8 +37,15 @@ function showDetails() {
   }, 50);
 }
 
+/*
+ * addKeyPressHandler
+ * 
+ * When the ESC key is pressed, hide the details
+ *
+ */
 function addKeyPressHandler() {
   'use strict';
+
   document.body.addEventListener('keyup', function(event) {
     event.preventDefault();
     if (event.keyCode === ESC_KEY) {
@@ -31,44 +54,14 @@ function addKeyPressHandler() {
   });
 }
 
-function addThumbClickHandler(thumb) {
-  'use strict';
-  thumb.addEventListener('click', function(event) {
-    event.preventDefault();
-    setDetailsFromThumb(thumb);
-    showDetails();
-  });
-}
-
-function getThumbnailsArray() {
-  'use strict';
-  var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
-  var thumbnailArray = [].slice.call(thumbnails);
-  return thumbnailArray;
-}
-
-function initializeEvents() {
-  'use strict'
-  var thumbnails = getThumbnailsArray();
-  thumbnails.forEach(addThumbClickHandler);
-  addKeyPressHandler();
-}
-
-function imageFromThumb(thumbnail) {
-  'use strict';
-  return thumbnail.getAttribute('data-image-url');
-}
-
-function titleFromThumb(thumbnail) {
-  'use strict';
-  return thumbnail.getAttribute('data-image-title');
-}
-
-function setDetailsFromThumb(thumbnail) {
-  'use strict';
-  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
-}
-
+/*
+ * setDetailsFromThumb
+ * 
+ * A convienence function which wraps a single call to setDetails,
+ * and calls both imageFromThumb and titleFromThumb based on the 
+ * thumb
+ *
+ */
 function setDetails(imageUrl, titleText) {
   'use strict';
 
@@ -79,4 +72,93 @@ function setDetails(imageUrl, titleText) {
   detailTitle.textContent = titleText;
 }
 
+/*
+ * imageFromThumb
+ * 
+ * Get the img url from the thumb
+ *
+ */
+function imageFromThumb(thumb) {
+  'use strict';
+  return thumb.getAttribute('data-image-url');
+}
+
+/*
+ * titleFromThumb
+ * 
+ * Get the text title from the thumb
+ *
+ */
+function titleFromThumb(thumb) {
+  'use strict';
+  return thumb.getAttribute('data-image-title');
+}
+
+/*
+ * setDetailsFromThumb
+ * 
+ * A convienence function which wraps a single call to setDetails,
+ * and calls both imageFromThumb and titleFromThumb based on the 
+ * thumb
+ *
+ */
+function setDetailsFromThumb(thumb) {
+  'use strict';
+
+  setDetails(imageFromThumb(thumb), titleFromThumb(thumb));
+}
+
+/*
+ * addThumbClickHandler
+ * 
+ * Add mouse click listener to the thumb.
+ *
+ */
+function addThumbClickHandler(thumb) {
+  'use strict';
+  
+  // Add the listener to the thum
+  thumb.addEventListener('click', function(event) {
+    // Each time the listener is called, set the details image to the 
+    // image and title from the thumbnail
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+    showDetails();
+  });
+}
+
+/*
+ * getThumbnailsArray
+ * 
+ * Returns an array of elements that contain 'data-image-role="trigger"'
+ *
+ */
+function getThumbnailsArray() {
+  'use strict';
+  // Get all tags that have '[data-image-role="trigger"]'
+  var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+
+  // Convert 'thumbnails' node list to array
+  var thumbnailArray = [].slice.call(thumbnails);
+  return thumbnailArray;
+}
+
+/*
+ * initializeEvents
+ * 
+ * Top level function. Getst the array of all thumbnail images and attaches
+ * mouse listeners to each.
+ *
+ */
+function initializeEvents() {
+  'use strict'
+
+  var thumbnails = getThumbnailsArray();
+  thumbnails.forEach(addThumbClickHandler);
+
+  // Add keypress handler tied to ESC to hide details when ESC is pressed
+  addKeyPressHandler();
+}
+
+// Main entry point
 initializeEvents();
